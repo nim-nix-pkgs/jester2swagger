29359d21c1b04390d049c02fc954649756525b59
hugosenari <hugosenari@gmail.com>
2022-03-24 20:25:17 -0300

    feat(nimpkgs): remove lock

--- main/flake.nix
+++ main/flake.nix
@@ -25,10 +25,13 @@
   let 
     lib  = flakeNimbleLib.lib;
     args = ["self" "nixpkgs" "flakeNimbleLib" "src-jester2swagger-main"];
-  in lib.mkRefOutput {
+    over = if builtins.pathExists ./override.nix 
+           then { override = import ./override.nix; }
+           else { };
+  in lib.mkRefOutput (over // {
     inherit self nixpkgs ;
     src  = deps."src-jester2swagger-main";
     deps = builtins.removeAttrs deps args;
     meta = builtins.fromJSON (builtins.readFile ./meta.json);
-  };
+  } );
 }
